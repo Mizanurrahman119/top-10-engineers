54a4e2b3357c0e2ff93ac128a7cf4928df0b4e26
Mizanurrahman119 <niloy719119@gmail.com>
2021-09-26 23:32:03 +0600

    added hire

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -11,6 +11,10 @@ const Cart = (props) => {
         <div>
             <h2>Added Engineers: {props.cart.length}</h2>
                 <h2>Total Salary: ${total}</h2>
+                {
+                    cart.map(cart => <h3 style={{backgroundColor:'khaki', textAlign:'center',padding:'5px'}}>{cart.name}</h3>)
+                }
+                <button style={{backgroundColor:'lightblue'}}>Hire Now</button>
         </div>
     );
 };
