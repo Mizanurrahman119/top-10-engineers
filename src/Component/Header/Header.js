7c9eb3bda496e5cc80fca0825ef0615618b53829
Mizanurrahman119 <niloy719119@gmail.com>
2021-12-03 00:43:20 +0600

    header addit

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -6,8 +6,7 @@ const Header = (data) => {
         <div className="header-container">
             <h1>Top 10 Engineers In World</h1>
             <p>AI and the future of engineering: an interview with Hugo Malagon</p>
-            <h1>Total Salay: 150 Million</h1>
-            <h2>people:{data.length}</h2>
+            <h1>Total Salary: 150 Million</h1>
         </div>
     );
 };
